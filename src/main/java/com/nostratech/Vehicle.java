864fe65d8fa03db6830465c859f993b0bc80f02d
ekasp20 <esptrgit92@gmail.com>
2024-04-02 10:39:33 +0700

    checkpoint 1

---
+++ src/main/java/com/nostratech/Vehicle.java
@@ -0,0 +1,13 @@
+package com.nostratech;
+
+public class Vehicle {
+	private String type;
+	private String regNo;
+	private String color;
+	
+	public Vehicle(String type, String regNo, String color) {
+		
+	}
+}
+
+
